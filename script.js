9fc2c32bf9ddd8b1b3f4982e855911c90d9267bf
Usman-Khan0 <musmankghouri@gmail.com>
2023-09-26 03:02:24 +0500

    Page-2 is Add

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-function loco() {
-    gsap.registerPlugin(ScrollTrigger);
+var tl2 = gsap.timeline();
 
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-    const locoScroll = new LocomotiveScroll({
-        el: document.querySelector(".main"),
-        smooth: true
-    });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-    locoScroll.on("scroll", ScrollTrigger.update);
-
-    // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy(".main", {
-        scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-        getBoundingClientRect() {
-            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-        },
-        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-    });
-
-
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-    ScrollTrigger.refresh();
-
-
-}
-loco()
-
+tl2
+gsap.from(".page-2 .h5", {
+    opacity: 0,
+    top: 20,
+    scrollTrigger: {
+        trigger: ".page-2 .h5",
+        scroller: "body",
+        start: "top 80%",
+        // end: "bottom center",
+        // markers: true,
+    }
+})
+gsap.from(".page-2 .h1", {
+    opacity: 0,
+    top: 40,
+    scrollTrigger: {
+        trigger: ".page-2 .h1",
+        scroller: "body",
+        start: "top 75%",
+        // end: "bottom center",
+        // markers: true,
+    }
+})
+gsap.from(".page-2 .btn-3", {
+    opacity: 0,
+    top: -20,
+    scrollTrigger: {
+        trigger: ".page-2 .btn-3",
+        scroller: "body",
+        start: "top 90%",
+        // end: "bottom center",
+        // markers:true,
+    }
+})
 
 
 var tl = gsap.timeline();
@@ -99,22 +100,28 @@ tl
         duration: 1.2,
         ease: "Power4.easeOut"
     })
-    .from('.icon-2', {
-        scale: 0.1,
-        opacity: 0,
-        delay: 0.1,
-    })
-    .from('.icon-1', {
-        scale: 0.1,
-        opacity: 0,
-        delay: 0.1,
-    })
+// .from('.icon-2', {
+//     scale: 0.1,
+//     opacity: 0,
+//     delay: 0.1,
+// })
+// .from('.icon-1', {
+//     scale: 0.1,
+//     opacity: 0,
+//     delay: 0.1,
+// })
 
 
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.classList.add("no-scroll");
 
+    setTimeout(function () {
+        document.body.classList.remove("no-scroll");
+    }, 6800);
+});
 
 
 
